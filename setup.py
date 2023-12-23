caf453c9bda04ab40fd424463cef893c8206f77b
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-23 19:14:27 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup, Extension
- 
-module1 = Extension('spi', sources = ['spi.c'])
- 
-setup (name = 'PackageName',
-        version = '1.0',
-        description = 'This is a demo package',
-        ext_modules = [module1])
+from setuptools import setup, Extension
+
+module1 = Extension('spi', sources=['spi.c'])
+
+setup(
+    name='PackageName',
+    version='1.0',
+    description='This is a demo package',
+    ext_modules=[module1],
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/PackageName',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: C',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
