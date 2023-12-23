37f96e2219e0a5c1e2243183b2d0addb8869291d
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-23 19:12:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 '''
-Created on 2013-6-20
+Created on 2023-12-23
 
-@author: Administrator
+@author: Billy
 '''
 
 import sys
-sys.path.insert(0, '/home/pi/RF905-Py')
+from time import sleep
 import RF905_Py
 
-rfn905 = RF905_Py.RF905(24,7,15,13,11,12,8)   #set  raspberryPi pin
-rfn905.writeconfig(0xCC,0xCC,0xCC,0xCC)       #set  sender RF905 address
-#rfn905.sendstr("hello",0xCC,0xCC,0xCC,0xCC,) #set  Receive Rf905 address
+try:
+    sys.path.insert(0, '/home/pi/RF905-Py')
 
-def callback(str):
-    print(str)
-    
-rfn905.listen(callback)
+    rfn905 = RF905_Py.RF905(24, 7, 15, 13, 11, 12, 8)   # set Raspberry Pi pin
+    rfn905.writeconfig(0xCC, 0xCC, 0xCC, 0xCC)         # set sender RF905 address
+
+    def callback(received_string):
+        print(received_string)
+
+    rfn905.listen(callback)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
